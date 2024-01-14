e1bd4704be52d2acd698a9344bf132a9cd81b04e
Kiran Dehlikar <149475012+kiran-opstree@users.noreply.github.com>
2024-01-14 13:43:43 +0530

    Update foo.groovy

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,3 +1,23 @@
 def call(String name = 'Ninjas') {
     echo "Hello, ${name}."
 }
+
+def unitTest() {
+    stage('Unit Testing') {
+        sh 'mvn checkstyle:checkstyle'
+        sh 'mvn findbugs:findbugs'
+        sh 'mvn pmd:pmd'
+    }
+}
+
+def dependencyCheck() {
+    stage('Dependency Check') {
+        sh 'mvn dependency-check:check'
+    }
+}
+
+def codeCoverage() {
+    stage('Code Coverage') {
+        sh 'mvn cobertura:cobertura '
+    }
+}
