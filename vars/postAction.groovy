302e4d6561cf9ea492778afdb58ee7839bd6077e
Kiran Dehlikar <149475012+kiran-opstree@users.noreply.github.com>
2024-01-14 17:50:46 +0530

    Update postAction.groovy

--- vars/postAction.groovy
+++ vars/postAction.groovy
@@ -1,11 +1,15 @@
 def call() {
-    try {
+    script {
+        try {
         currentBuild.resultIsBetterOrEqualTo('SUCCESS')
         // Notify on success
         slackSend channel: '#pipeline-dec', color: 'good', message: "Job '${env.JOB_NAME}' Success!", teamDomain: 'kiran-workspacegroup', tokenCredentialId: 'df4374b7-51c5-4d43-8297-a5e1971ef969'
-    } catch (Exception e) {
+        } 
+        catch (Exception e) {
         // Notify on failure
         slackSend channel: '#pipeline-dec', color: 'good', message: "Job '${env.JOB_NAME}' Success!", teamDomain: 'kiran-workspacegroup', tokenCredentialId: 'df4374b7-51c5-4d43-8297-a5e1971ef969'
         throw e
+        }
     }
 }
+    
